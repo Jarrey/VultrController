b4ca1d51c70f6850afe740493c3902b2f66e4652
Jarrey <jar.bob@gmail.com>
2020-02-26 10:26:12 +0800

    Add argument to choose region

--- VultrCtl.py
+++ VultrCtl.py
@@ -9,7 +9,7 @@ import urllib.parse
 import urllib.request
 from urllib.error import HTTPError
 
-TARGET_REGION = random.choice(['Sydney'])
+TARGET_REGION = ['Sydney', 'Tokyo', 'Singapore']
 TARGET_PLAN = 5 # $5.00 per month
 
 SERVER_LIST = 'https://api.vultr.com/v1/server/list'
@@ -62,10 +62,10 @@ def get_current_snaprshots(api_key):
     return get(SNAPSHOT_LIST, api_key)
 
 
-def get_target_region():
+def get_target_region(target_region):
     regions = get(REGIONS_LIST)
     for region in regions.values():
-        if region['name'] == TARGET_REGION:
+        if region['name'] == target_region:
             return region['DCID']
 
 
@@ -87,9 +87,9 @@ def destroy(api_key, sub_id):
     post(DESTROY, api_key, {'SUBID': sub_id})
 
 
-def deploy(api_key):
+def deploy(api_key, region):
     data = {
-        'DCID': get_target_region(),
+        'DCID': get_target_region(region),
         'VPSPLANID': get_target_plan(),
         'OSID': get_snapshot_os(),
         'SNAPSHOTID': next(iter(get_current_snaprshots(api_key))),
@@ -98,19 +98,32 @@ def deploy(api_key):
     return post(CREATE_VPS, api_key, data)
 
 
+def choice_region(arg):
+    arg = arg.lower()
+    if arg == 'sy' or arg == 'sydney':
+        return 'Sydney'
+    elif arg == 'tk' or arg == 'tokyo':
+        return 'Tokyo'
+    elif arg == 'sg' or arg == 'singapore':
+        return'Singapore'
+    else:
+        return random.choice(TARGET_REGION)
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Vultr VPS controller tool')
     parser.add_argument('-k', '--key', required=True, help='API Key of Vultr account', action='store')
+    parser.add_argument('-r', '--region', choices=['Sydney', 'Tokyo', 'Singapore'], help='VPS region to deploy, default is rendom choice from build-in list', action='store')
     parser.add_argument('-a', '--action', required=True, choices=['d', 'r'], help='Action for Vultr VPSs, "destroy" or "renew"]', action='store')
     parser.add_argument('-d', '--destroy', help='Whether destroy the legacy VPS instance when renew VPS', action='store_true')
 
     args = parser.parse_args()
     api_key = args.key
-
+    region = choice_region(args.region)
     vpss = get_current_vpses(api_key)
 
     if args.action == 'r':
-        deploy(api_key)
+        deploy(api_key, region)
 
     if vpss:
         for vps in vpss.values():
